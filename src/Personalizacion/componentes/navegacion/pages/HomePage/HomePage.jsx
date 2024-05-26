afe41c09847e196077b036b8fece39bef9d76512
MADPARADOX431 <miguelitoc1@outlook.com>
2024-05-26 13:11:03 -0500

    se agregaron las carpetas Home carrosel y Page

---
+++ src/Personalizacion/componentes/navegacion/pages/HomePage/HomePage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import MainCarrosel from '../../../../Home carrosel/MainCarrosel'
+
+const HomePage = () => {
+  return (
+    <div>
+        <MainCarrosel/>
+
+        <div>
+            Other Section
+        </div>
+    </div>
+  )
+}
+
+export default HomePage;
