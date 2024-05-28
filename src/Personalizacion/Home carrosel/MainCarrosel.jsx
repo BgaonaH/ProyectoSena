886f30af363361249262c2cde1e275bd894ec5dc
Benjamin <psy.egaona@gmail.com>
2024-05-27 20:28:50 -0500

    Se implemento un carusel

--- src/Personalizacion/Home carrosel/MainCarrosel.jsx
+++ src/Personalizacion/Home carrosel/MainCarrosel.jsx
@@ -1,19 +1,23 @@
 import React from 'react';
 import AliceCarousel from 'react-alice-carousel';
 import 'react-alice-carousel/lib/alice-carousel.css';
+//import '../../App.css';
 
 const responsive = {
     0: { items: 1 },
     568: { items: 2 },
-    1024: { items: 3 },
+    1024: { items: 3
+
+     },
 };
 
 const items = [
-    <div className="item" data-value="1">1</div>,
-    <div className="item" data-value="2">2</div>,
-    <div className="item" data-value="3">3</div>,
-    <div className="item" data-value="4">4</div>,
-    <div className="item" data-value="5">5</div>,
+    
+    <div className="item" data-value="1"><img src="https://i.imgur.com/yg1jILy.jpg" role="presentation"/></div>,
+    <div className="item" data-value="2"><img src="https://i.imgur.com/CIQQ1tf.png"/></div>,
+    <div className="item" data-value="3"><img src="https://i.imgur.com/uqB124i.jpg"/></div>,
+    <div className="item" data-value="4"><img src="https://i.imgur.com/T6trwx1.png"/></div>,
+    <div className="item" data-value="5"><img src="https://i.imgur.com/Efb9oiY.png"/></div>,
 ];
 
 const MainCarrosel = () => (
