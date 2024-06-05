27e89a225f5f96d53249c69a0fc6cd000964a7ff
MADPARADOX431 <miguelitoc1@outlook.com>
2024-06-04 23:16:35 -0500

    Se definieron las condiciones para que el boton de Mover las imagenes desaparezca tras recorrer el carrusel, pero esta incompleto

--- src/Personalizacion/componentes/navegacion/HomeSectionCarrusel/HomeSectionCarrusel.jsx
+++ src/Personalizacion/componentes/navegacion/HomeSectionCarrusel/HomeSectionCarrusel.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import AliceCarousel from 'react-alice-carousel';
 import HomeSectionCard from '../HomeSectionCard/HomeSectionCard';
 import Button from '@mui/material/Button';  // Importar el botón correctamente
@@ -6,14 +6,19 @@ import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft';
 import { Rotate90DegreesCwSharp } from '@mui/icons-material';
 
 const HomeSectionCarrusel = () => {
+    const [activeIndex,setActiveIndex]=useState(0);
     const responsive = {
         0: { items: 1 },
         720: { items: 3 },
-        1024: { items: 5.5},
+        1024: { items: 5.5 },
     };
+    const slidePrev=()=>setActiveIndex(activeIndex-1);
+    const slideNext=()=>setActiveIndex(activeIndex+1);
 
+    const syncActiveIndex= ({item})=>syncActiveIndex(item)
     const items = [1, 1, 1, 1, 1].map((item, index) => <HomeSectionCard key={index} />);  // Agregar una key única a cada elemento
 
+    
     return (
         <div className="px-4 lg:px-8 border border-black">
             <div className="relative p-5 border">
@@ -23,45 +28,49 @@ const HomeSectionCarrusel = () => {
                     infinite
                     responsive={responsive}
                     disableDotsControls
+                    onSlideChange={syncActiveIndex}
+                    activeIndex={activeIndex}
                 />
-                 <Button
+                {activeIndex !== items.length-5 && <Button
                     variant="contained"
                     className="z-50 bg white"
-                    sx= {{
-                        position:"absolute",
+                    onClick={slideNext}
+                    sx={{
+                        position: "absolute",
                         top: '8rem',
                         right: '0rem',
-                        transform:"translate(50%) rotate(90deg)",
+                        transform: "translate(50%) rotate(90deg)",
                         Index: 50,
-                        bgcolor:"white",
-                        
-                        
+                        bgcolor: "white",
+
+
                     }}
-                        
-                        aria-label="next"
-                    
+
+                    aria-label="next"
+
                 >
-                    <KeyboardArrowLeftIcon sx={{transform:"rotate(90deg)",color:"black"}} />
-                </Button>
+                    <KeyboardArrowLeftIcon sx={{ transform: "rotate(90deg)", color: "black" }} />
+                </Button>}
 
                 <Button
+                onClick={slidePrev}
                     variant="contained"
                     className="z-50 bg white"
-                    sx= {{
-                        position:"absolute",
+                    sx={{
+                        position: "absolute",
                         top: '8rem',
                         left: '0rem',
-                        transform:"translate(50%) rotate(90deg)",
+                        transform: "translate(50%) rotate(90deg)",
                         Index: 50,
-                        bgcolor:"white",
-                        
-                        
+                        bgcolor: "white",
+
+
                     }}
-                        
-                        aria-label="next"
-                    
+
+                    aria-label="next"
+
                 >
-                    <KeyboardArrowLeftIcon sx={{transform:"rotate(270deg)",color:"black"}} />
+                    <KeyboardArrowLeftIcon sx={{ transform: "rotate(270deg)", color: "black" }} />
                 </Button>
             </div>
         </div>
