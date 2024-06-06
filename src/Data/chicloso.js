144907e581f92d3b97310693eab16aaa27d5d286
Benjamin <psy.egaona@gmail.com>
2024-06-06 08:45:55 -0500

    se corigio archivo jason para los datos del carousel

---
+++ src/Data/chicloso.js
@@ -0,0 +1,82 @@
+export const chicloso = [
+    {
+        "imageUrl": "https://i.imgur.com/5len0cX.jpg",
+        "brand": "Artesanales",
+        "title": "Turrón tradicional Colombiano",
+        "peso": "250gr",
+        "discountprice": 500,
+        "price": 650,
+        "discountpersent": 23,
+        "descripcion": "El Turrón, una delicia que viaja desde las tierras mediterráneas hasta las montañas colombianas, trae consigo un legado de sabores ancestrales. Este exquisito manjar, originalmente creado en las regionesde Italia y Francia, ha encontrado un nuevo hogar en Colombia, donde se ha fusionado armoniosamente con las recetas tradicionales de la pastelería suiza. Con una combinación única de miel, azúcar, maní y café, el Turrón colombiano se presenta en una variedad de texturas, desde suaves y esponjosas hasta firmes y crujientes. Su proceso de elaboración se mantiene fiel a las técnicas artesanales, honrando así su autenticidad y herencia culinaria.Sumérgete en el mundo de los sabores con este dulce tesoro, donde cada bocado es un viaje sensorial a través de la rica historia gastronómica de Colombia."
+    },
+    {
+        "imageUrl": "https://i.imgur.com/T6trwx1.png",
+        "brand": "Dulces del valle",
+        "title": "Marqueta combinada DULCES DEL VALLE",
+        "peso": "210gr",
+        "discountprice": 8162,
+        "price": 10600,
+        "discountpersent": 23,
+        "descripcion": "Sumérgete en la experiencia sensorial de nuestro dulce artesanal de guayaba, un manjar que encarna la esencia misma del sabor colombiano. Cada bocado es una explosión de sabores auténticos y frescos que te transportarán a los campos frutales de nuestra tierra. Con su textura delicadamente elaborada y su sabor inigualable, este dulce te ofrece un momento de placer gastronómico dondequiera que estés. Disfruta de la magia de Colombia con cada mordisco y déjate seducir por la dulzura de nuestra tradición culinaria."
+    },
+    {
+        "imageUrl": "https://i.imgur.com/T6trwx1.png",
+        "brand": "SuperCoco",
+        "title": "Turron SuperCoco",
+        "peso": "250gr",
+        "discountprice": 6460,
+        "price": 8390,
+        "discountpersent": 23,
+        "descripcion": "El original turrón con trozos de coco 100% natural. Este tradicional dulce es perfecto para regalar, para llevar de viaje, o incluso para mantener en casa."
+    },
+    {
+        "imageUrl": "https://i.imgur.com/CIQQ1tf.png",
+        "brand": "Colobina",
+        "title": "Chomelos",
+        "peso": "90gr",
+        "discountprice": 5621,
+        "price": 7300,
+        "discountpersent": 23,
+        "descripcion": "La perfecta combinación de un delicioso Masmelo de vainilla, sobre una crocante galleta, bañado con exquisito Chocolate."
+    },
+    {
+        "imageUrl": "https://i.imgur.com/hlQrWr2.png",
+        "brand": "Italo",
+        "title": "Masmelos",
+        "peso": "50gr",
+        "discountprice": 4165,
+        "price": 5410,
+        "discountpersent": 23,
+        "descripcion": "Dulce esponjoso hecho con clara de huevo batida, leche y azúcar, de diversas formas, tamaños y colores."
+    },
+    {
+        "imageUrl": "https://i.imgur.com/32P8WoJ.png?1",
+        "brand": "Italo",
+        "title": "Moritas",
+        "peso": "850gr",
+        "discountprice": 29683,
+        "price": 38550,
+        "discountpersent": 23,
+        "descripcion": "Gomitas con sabor a mora y grageadas con azúcar. Nueva presentación con el sabor tradicional e inigualable."
+    },
+    {
+        "imageUrl": "https://i.imgur.com/Eqp25KH.png",
+        "brand": "Italo",
+        "title": "Uvas cubiertas de chocolate",
+        "peso": "40gr",
+        "discountprice":  3619,
+        "price": 4700,
+        "discountpersent": 23,
+        "descripcion": "as uvas cubiertas con chocolate de leche hacen la combinación perfecta"
+    },
+    {
+        "imageUrl": "https://i.imgur.com/TWuXBvj.png?1",
+        "brand": "Italo",
+        "title": "Caramelo de leche",
+        "peso": "520gr",
+        "discountprice":  10626,
+        "price": 13800,
+        "discountpersent": 23,
+        "descripcion": "Delicioso caramelo de leche, masticable y suave al paladar."
+    },
+]
