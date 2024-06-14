ca09ef3c80280efe0be8361b944de621aad6ab06
Benjamin <psy.egaona@gmail.com>
2024-06-13 20:36:47 -0500

    Se hizo los efecto de la tarjeta y se empeiuza aplatear el grid de product

--- src/Personalizacion/componentes/Product/ProductCard.jsx
+++ src/Personalizacion/componentes/Product/ProductCard.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
-
+import './ProductCard.css'
 const ProductCard = () => {
   return (
-    <div className='productCard w-[15rem] m-3 transition-all cursosr pointer' >
+    <div className='productCard w-[15rem] m-3 transition-all cursosr pointer p-1' >
         <div className='h-20rem'>
             <img className='h-[15rem] w-[13rem] object-cover object-left-top' 
             src="https://i.imgur.com/32P8WoJ.png?1" alt="" />
@@ -12,7 +12,7 @@ const ProductCard = () => {
             <p className=''>Gomitas con sabor a mora y grageadas con azúcar
                 marca italo </p>
         </div>
-        <div className='flex items-center space-x-2'>
+        <div className='textPart flex items-center space-x-2 pl-10'>
             <p className='font-semibold opacity-50'>29.683</p>
             <p className='line-through opacity-50'>38.550 </p>
             <p className='text-green-600 font-semibold'>23% off</p>
--- src/Personalizacion/pages/Homepage/HomePage.jsx
+++ src/Personalizacion/pages/Homepage/HomePage.jsx
@@ -1,23 +1,23 @@
 import React from "react";
 import MainCarrosel from "../../componentes/Home carrosel/MainCarrosel";
 import HomeSectionCarrusel from "../../componentes/HomeSectionCarrusel/HomeSectionCarrusel";
-import { chicloso } from "../../../Data/chicloso";
-import { dulces } from "../../../Data/dulces";
-import { galletas } from "../../../Data/galletas";
+import { chicloso } from "../../Data/chicloso";
+import { dulces } from "../../Data/dulces";
+import { galletas } from "../../Data/galletas";
+
 
 const HomePage = () => {
     return (
       <div>
-          <MainCarrosel/>
-          
+          <MainCarrosel/>z
           <div className="space-y-10 py-20 flex flex-col justify-center px-5 lg:px-10">
               <HomeSectionCarrusel className="text-lg font-medium text-gray-900 line-clamp-1" data={chicloso} sectionName={"Chiclosos"}/>
               <HomeSectionCarrusel data={dulces} sectionName={"Dulces duros"}/>
-              <HomeSectionCarrusel data={galletas} sectionName={"Galletas"}/>
               <HomeSectionCarrusel data={chicloso}/>
               <HomeSectionCarrusel data={chicloso}/>
               
           </div>
+          
       </div>
     )
   }
